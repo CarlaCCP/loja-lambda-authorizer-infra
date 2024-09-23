436dbe90494ae09f9de6e82f0bee04da984d40cc
carla-gb <carla.paula@grupoboticario.com.br>
2024-09-23 19:05:16 -0300

    init terraform

---
+++ lambda.tf
@@ -0,0 +1,24 @@
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "lambda.js"
+  output_path = "lambda_function_payload.zip"
+}
+
+resource "aws_lambda_function" "loja-authorizer" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_loja_authorizer"
+  role          = "arn:aws:iam::019248244455:role/LabRole"
+  handler       = "app.lambda_handler"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "python3.9"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
